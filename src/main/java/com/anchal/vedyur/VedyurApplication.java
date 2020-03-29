5d2bb219a25ec17a0b7de4a804b1dd9ebbb4afbb
Avinash Anchal <anchalavinash66@gmail.com>
2020-03-29 13:04:48 +0530

    Initial commit

---
+++ src/main/java/com/anchal/vedyur/VedyurApplication.java
@@ -0,0 +1,13 @@
+package com.anchal.vedyur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VedyurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VedyurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anchal/vedyur/VedyurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anchal.vedyur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VedyurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
